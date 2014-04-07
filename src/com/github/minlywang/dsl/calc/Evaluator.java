d9d2c96f665589d16fecb568f0fd40aa8978b55a
minly <dolphinwang999@yahoo.com>
2014-04-07 18:26:09 +0800

    add license

--- src/com/github/minlywang/dsl/calc/Evaluator.java
+++ src/com/github/minlywang/dsl/calc/Evaluator.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014 Jake Wang
+ * https://github.com/minlywang/calc
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ */
 package com.github.minlywang.dsl.calc;
 
 import java.util.ArrayList;
